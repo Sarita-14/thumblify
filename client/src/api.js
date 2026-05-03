4cf5a4c892fbc85b5a21b78da45128bf5f63ec64
Sarita-14 <korisarita.1404@gmail.com>
2026-05-03 21:01:33 +0530

    force fix api url

--- client/src/api.js
+++ client/src/api.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'https://thumblify-p0f78zgsc-sarita-14s-projects.vercel.app',
+  headers: {
+    'Content-Type': 'application/json',
+  }
 })
 
 export default api
